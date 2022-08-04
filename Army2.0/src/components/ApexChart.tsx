e41de46588a152e46cedc2b4a5227fd533296cbb
DanielRotnemer <traderrt955@gmail.com>
2022-08-04 14:58:30 +0300

    graphs stage one

--- Army2.0/src/components/ApexChart.tsx
+++ Army2.0/src/components/ApexChart.tsx
@@ -1,36 +1,164 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { useQuery } from "react-query";
 import ReactApexChart from 'react-apexcharts';
-import Props from 'react-apexcharts';
+import { stringify } from 'querystring';
+import { color } from '@mui/system';
 
-const fetchUsers = async () => {
-  const res = await fetch("../../../api-server/RCGW/json files/statistics/pingerdb.json");
+interface State {
+  time: string;
+  state: string;
+}
+
+interface StateRecord {
+  [key: string]: {[ip: string]: Array<State>}
+}
+
+interface SeriesItem {
+  name: string;
+  data: Array<{x: string; y: Array<Number>;}>;
+}
+
+const temp: StateRecord = 
+{
+  "Unit99": 
+  {
+    "A.A.A.A": [{"time": "04/08/2022 09:05:36", "state": "down"}, {"time": "04/08/2022 09:32:36", "state": "up"}, {"time": "04/08/2022 10:05:36", "state": "down"}, {"time": "4/8/2022 10:32:36", "state": "up"}],
+    "B.B.B.B": [{"time": "04/08/2022 10:05:36", "state": "down"}, {"time": "04/08/2022 10:32:36", "state": "up"}],
+    "C.C.C.C": [{"time": "04/08/2022 10:05:36", "state": "down"}, {"time": "04/08/2022 10:32:36", "state": "up"}],
+    "D.D.D.D": [{"time": "04/08/2022 10:05:36", "state": "down"}, {"time": "04/08/2022 10:32:36", "state": "up"}],
+    "E.E.E.E": [{"time": "04/08/2022 10:05:36", "state": "up"}, {"time": "04/08/2022 10:32:36", "state": "down"}],
+    "F.F.F.F": [{"time": "04/08/2022 10:05:36", "state": "up"}, {"time": "04/08/2022 10:32:36", "state": "down"}],
+    "G.G.G.G": [{"time": "04/08/2022 10:05:36", "state": "up"}, {"time": "04/08/2022 10:32:36", "state": "down"}],
+    "H.H.H.H": [{"time": "04/08/2022 10:05:36", "state": "up"}, {"time": "04/08/2022 10:32:36", "state": "down"}],
+    "I.I.I.I": [{"time": "04/08/2022 10:05:36", "state": "up"}, {"time": "04/08/2022 10:32:36", "state": "down"}]
+  }
+};
+
+const fetchUsers = async () => 
+{
+  const res = await fetch("../../../api-server/RCGW/json files/statistics/pingerdb.json"); 
   return res.json();
 };
 
-const ApexChart = () => {
-  const { data, status } = useQuery("users", fetchUsers);
-  
-  /*return (<div id="chart">
-    <ReactApexChart options={this.state.options} series={this.state.series} type="bar" height={350} />
-  </div>);*/
+const ApexChart = () => 
+{
+  const [series, setSeries] = useState(Array<SeriesItem>);
 
-  return (
-    <div>
-      {status === "error" && <p>Error fetching data</p>}
-      {status === "loading" && <p>Fetching data...</p>}
-      {status === "success" && (
-        <div>
-          {data.map((user: any) => (
-            <p key={user.id}>{user.name}</p>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-}
+  const [options, setOptions] = useState({
+    chart: {
+      height: 350,
+      type: 'rangeBar'
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        barHeight: '70%',
+        rangeBarGroupRows: true
+      }
+    },
+    colors: [
+      "#55FF55", "#FF5555"
+    ],
+    fill: {
+      type: 'solid'
+    },
+    xaxis: {
+      type: 'datetime',
+      labels: {
+        datetimeUTC: false
+      }
+    },
+    legend: {
+      position: 'right'
+    },
+    tooltip: {
+      custom: function(opts: any) {
+        const fromYear = new Date(opts.y1).getFullYear()
+        const toYear = new Date(opts.y2).getFullYear()
+        const values = opts.ctx.rangeBar.getTooltipValues(opts)    
+        return fromYear + " to " + toYear + " | " + values;
+      }
+    }
+  });
 
-//const domContainer = document.querySelector('#app');
-//ReactDOM.render(React.createElement(ApexChart), domContainer);
+  Object.keys(temp).map((unit: string) => 
+  {
+    var upSeriesItem: SeriesItem = 
+      {
+        name: "up " + ip,
+        data: []
+      };
+      var downSeriesItem: SeriesItem = 
+      {
+        name: "down " + ip,
+        data: []
+      };
+    Object.keys(temp[unit]).map((ip) => 
+    {
+      
+      temp[unit][ip].map((state) =>
+      {
+        var localTime = new Date();
+        var year = localTime.getFullYear();
+        var month = localTime.getMonth() + 1;
+        var day = localTime.getDate();
+        var hours = localTime.getHours();
+        var minutes = localTime.getMinutes();
+        var seconds = localTime.getSeconds();
+        // const [currentDateTime, setCurrentDateTime] = useState(day + "/" + month + "/" + year + " " + hours + ":" + minutes + ":" + seconds);
+        let currentDateTime = day + "/" + month + "/" + year + " " + hours + ":" + minutes + ":" + seconds;
+        // console.log(currentDateTime + " | " + state.time + " | " + state.state); 
+
+        let index = temp[unit][ip].indexOf(state);
+        let endStateTime = temp[unit][ip].length > index + 1 ? temp[unit][ip][index + 1].time : currentDateTime;
+        console.log(new Date(endStateTime).getTime() + " | " + endStateTime)
+        //console.log(unit + " : " + ip + " : {" + state.state + " : " + state.time + "}");
+        
+        if (state.state == "up")
+        {          
+          upSeriesItem.data.push({
+            x: ip,
+            y: [
+              new Date(state.time).getTime(),
+              new Date(endStateTime).getTime()
+            ]
+          });
+        }
+        else if (state.state == "down")
+        {
+          downSeriesItem.data.push({
+            x: ip,
+            y: [
+              new Date(state.time).getTime(),
+              new Date(endStateTime).getTime()
+            ]
+          });
+        }
+        /*=
+        {
+          name: state.state,
+          data: [
+            {
+              x: ip,
+              y: [
+                new Date(state.time).getTime(),
+                new Date(endStateTime).getTime()
+              ]
+            }
+          ]
+        };*/
+      });
+      console.log(upSeriesItem);
+      
+    });
+    series.push(upSeriesItem);
+    series.push(downSeriesItem);
+  });
+    
+  return (<div id="chart">
+    <ReactApexChart options={options} series={series} type="rangeBar" height={350} />
+  </div>);
+};
+
 export default ApexChart;
