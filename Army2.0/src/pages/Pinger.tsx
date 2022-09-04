d43b2819d732f50c0e073d3bfab335cef6c9e7e8
amiteli <elizura1@gmail.com>
2022-09-04 10:42:12 +0300

    pinger

--- Army2.0/src/pages/Pinger.tsx
+++ Army2.0/src/pages/Pinger.tsx
@@ -1,12 +1,20 @@
 // עמוד פינגר
 import React from "react";
-import ReactApexChart from 'apexcharts';
-import ApexChart from '../components/ApexChart';
+import ReactApexChart from "apexcharts";
+import ApexChart from "../components/ApexChart";
 
 type Props = {};
 
 const Pinger = (props: Props) => {
-  return <div>Pinger<ApexChart/></div>;
+  return (
+    <div>
+      Pinger
+      {/* <ApexChart /> */}
+      <a href="C:/Work/מערכת ניטור/Army2.0/Army2.0/src/images/all.png" download>
+        Click
+      </a>
+    </div>
+  );
 };
 
 export default Pinger;
